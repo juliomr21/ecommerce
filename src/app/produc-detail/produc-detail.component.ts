5584abf61afcdc5af2c4652d4cd9f589c5f2b429
juliomr21 <juliomr21.correo.com>
2022-10-19 15:12:10 -0400

    visul ok

--- src/app/produc-detail/produc-detail.component.ts
+++ src/app/produc-detail/produc-detail.component.ts
@@ -6,22 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produc-detail.component.css']
 })
 export class ProducDetailComponent implements OnInit {
-  cant: number = 0
+  cant: number = 0;
+  image_detail = "assets/image-product-1.jpg";
+  img_selec ={"border": "3px solid","border-color": "hsl(26, 100%, 55%)","opacity": "80%" }
+  img1 = {};
+  img2 = {};
+  img3 = {};
+  img4 = {};
   constructor() { }
 
   ngOnInit(): void {
   }
   modificar_cart( op:string){
-    //let op = 2;
+    
     if(op == "1")
     {
       this.cant++;
-
     }
     else
     {
       if(this.cant - 1 >= 0)
       this.cant--;
     }
+  }
+  change_image(op:string){
+    this.image_detail = "assets/image-product-"+op+".jpg";
+    this.img1 = {};
+    this.img2 = {};
+    this.img3 = {};
+    this.img4 = {};
+    switch(op){
+     case '1': this.img1 = this.img_selec;break
+     case '2': this.img2 = this.img_selec;break
+     case '3': this.img3 = this.img_selec;break
+     case '4': this.img4 = this.img_selec;break
+    }
+    
   }
 }
